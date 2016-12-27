31c8d76393e4640b19d46114c80d31ddf9d3ea49
Gerik13 <Gerik13@mail.ru>
2016-12-27 17:05:12 +0300

    Update AssemblyInfo.cs

--- LanguageTranslator/LanguageTranslator/Properties/AssemblyInfo.cs
+++ LanguageTranslator/LanguageTranslator/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("Version v.1.0")]
-[assembly: AssemblyFileVersion("Version v.1.")]
+[assembly: AssemblyFileVersion("Version v.1.0")]
